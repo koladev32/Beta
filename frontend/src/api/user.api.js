380b2e933c3ca9b2564549f381d36a0c6e2d71de
Onael21 <onaelmangabo@gmail.com>
2019-08-02 03:59:02 +0100

    Add login,logout and handleresponse function

---
+++ frontend/src/api/user.api.js
@@ -0,0 +1,50 @@
+import authHeader from '../_helpers/auth-header.js'
+import axios from "axios"
+
+
+export const userService = {
+    login,
+    logout,
+    getAll
+};
+
+function login(email,password){
+    const requestOptions = {
+        method : 'POST',
+        headers : {'Content-Type':'application/json'},
+        body:JSON.stringify({email,password})
+    };
+
+    return axios.post('http://localhost:8000/api/users/login/', this.requestOptions)
+    .then(handleResponse)
+    .then(user => {
+        if (user.token){
+            localStorage.setItem('user',JSON.stringify(user));
+        }
+    })
+}
+
+function logout(){
+    localStorage.removeItem('user');
+}
+
+function getAll(){
+
+}
+
+function handleResponse(response){
+    return response.text().then( text => {
+        const data = text && JSON.parse(text);
+        if (!response.ok){
+            if (response.status === 401){
+                logout();
+                location.reload(true);
+            }
+
+            const error = (data && data.message) || response.statusText;
+            return Promise.reject(error);
+        }
+
+        return data;
+    });
+}
--- reseaut/reseaut/settings.py
+++ reseaut/reseaut/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'jo)3-w-xn-=bm024demiq*cyo2=h6p^g%6$i)$ve&ncl4g+*67'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','172.20.10.2:8081']
 
 
 # Application definition
